b247dc84115cbebd80e019c35729eea5dee33972
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-02-17 14:05:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
    if array.size == 1 
      array.join(" and ")
-   elsif 
-   array.size == 3
-    array.join(", ")
+   elsif array.size == 2
+    return array.join(" and ")
    end
  end
